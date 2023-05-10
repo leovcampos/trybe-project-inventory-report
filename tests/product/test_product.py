e558494a92e006d85ccf5e7e1261e0228e13e99d
leovcampos <leonardoviniciuscampos@gmail.com>
2023-05-10 12:48:37 -0300

    req01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,9 +3,9 @@ from inventory_report.inventory.product import Product
 
 def test_cria_produto():
     product = Product(
-        '1',
-        'Empresa',
+        1,
         'Produto',
+        'Empresa',
         '10/06/2023',
         '10/06/2024',
         '1234',
@@ -16,5 +16,5 @@ def test_cria_produto():
     assert product.nome_da_empresa == "Empresa"
     assert product.data_de_fabricacao == "10/06/2023"
     assert product.data_de_validade == "10/06/2024"
-    assert product.numero_de_serie == 1234
+    assert product.numero_de_serie == '1234'
     assert product.instrucoes_de_armazenamento == "Cuidado, frágil"
