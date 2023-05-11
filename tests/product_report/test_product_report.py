d3db67bf5b782741583b36108b2b717991203dbb
leovcampos <leonardoviniciuscampos@gmail.com>
2023-05-11 15:08:50 -0300

    test req08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        '1',
+        'Product',
+        'Company',
+        '10/06/2023',
+        '10/06/2024',
+        '1234',
+        'em estado congelado')
+
+    assert str(product) == """O produto Product fabricado em 10/06/2023 por
+    Company com validade até 10/06/2024
+    precisa ser armazenado em estado congelado."""
