039c23c325e5f7fd67bb188f50903e6c6cf98f9b
leovcampos <leonardoviniciuscampos@gmail.com>
2023-05-11 15:00:52 -0300

    req03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    def generate(list):
+        report = ""
+
+        companies = Counter([item["nome_da_empresa"] for item in list])
+        for company, times in companies.items():
+            report += f"- {company}: {times}\n"
+
+        return (
+            f"{SimpleReport.generate(list)}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{report}"
+        )
