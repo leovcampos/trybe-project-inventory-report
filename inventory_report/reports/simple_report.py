328cddd9957c98c17364f013f16f18301963dd14
leovcampos <leonardoviniciuscampos@gmail.com>
2023-05-12 14:07:53 -0300

    lint

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -7,11 +7,12 @@ class SimpleReport:
         update_day = date.today()
         date_validity = []
 
-        oldest_fabrication_date = min(item["data_de_fabricacao"] for item in list)
+        old_fabrication_date = min(item["data_de_fabricacao"] for item in list)
 
         validity_date = [item["data_de_validade"] for item in list]
         list_validity_date = [
-            datetime.strptime(item, "%Y-%m-%d").date() for item in validity_date
+            datetime.strptime(item, "%Y-%m-%d").date()
+            for item in validity_date
         ]
 
         for item in list_validity_date:
@@ -21,7 +22,7 @@ class SimpleReport:
         companies = Counter([item["nome_da_empresa"] for item in list])
 
         return (
-            f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
+            f"Data de fabricação mais antiga: {old_fabrication_date}\n"
             f"Data de validade mais próxima: {min(date_validity)}\n"
             f"Empresa com mais produtos: {companies.most_common(1)[0][0]}"
         )
