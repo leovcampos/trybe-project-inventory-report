263fd4c19072352e9fee1cc36efd5abf25382c65
leovcampos <leonardoviniciuscampos@gmail.com>
2023-05-12 14:03:52 -0300

    req02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -7,22 +7,21 @@ class SimpleReport:
         update_day = date.today()
         date_validity = []
 
-        fabrication_date = [item["data_de_fabricacao"] for item in list]
-        f_fabrication_date = [
-            datetime.strptime(item, "%Y-%m-%d") for item in fabrication_date
-        ]
+        oldest_fabrication_date = min(item["data_de_fabricacao"] for item in list)
 
         validity_date = [item["data_de_validade"] for item in list]
-        f_validity_date = [
-            datetime.strptime(item, "%Y-%m-%d") for item in validity_date
+        list_validity_date = [
+            datetime.strptime(item, "%Y-%m-%d").date() for item in validity_date
         ]
 
-        for item in f_validity_date:
-            if f_validity_date >= update_day:
+        for item in list_validity_date:
+            if item >= update_day:
                 date_validity.append(item)
 
         companies = Counter([item["nome_da_empresa"] for item in list])
 
-        return f"""Data de fabricação mais antiga: {min(f_fabrication_date)}
-                Data de validade mais próxima: {min(date_validity)}
-                Empresa com mais produtos: {companies.most_common(1)[0]}"""
+        return (
+            f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
+            f"Data de validade mais próxima: {min(date_validity)}\n"
+            f"Empresa com mais produtos: {companies.most_common(1)[0][0]}"
+        )
